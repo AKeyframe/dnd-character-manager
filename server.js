2a57a09d42aebff977acc057ee1912a1530c956d
Keyframe <alec.kagebein@gmail.com>
2021-10-01 08:55:40 -0500

    auth-routes

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const methodOverride = require('method-override');
 const mongoose = require("mongoose");
 const app = express();
 const db = mongoose.connection;
-
+const session = require('express-session');
 
 const PORT = process.env.PORT || 3000;
-
 const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI, {
@@ -20,6 +19,10 @@ db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
 db.on('connected', () => console.log('Mongo Connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
+//Require Controllers
+const userController = require("./controllers/users");
+const sessionsController = require('./controllers/sessions');
+
 
 app.use(express.static('public'));
 
@@ -30,21 +33,28 @@ app.use(express.static('public'));
 // populates req.body with parsed info from forms
 // extended: false - does not allow nested objects in query strings
 app.use(express.urlencoded({ extended: false })); 
-
+app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
 // returns middleware that only parses JSON - may or may not need it depending on your project
 //app.use(express.json());
 
-app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false
+    }));
 
 
+app.use('/users', userController);
+app.use('/sessions', sessionsController);
 //=================================================================
 //                            Routes
 //==================================================================
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-});
+app.get("/", (req, res) => {
+	res.send("Root route");
+  });
 
 
 
